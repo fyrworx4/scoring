634766a56784416d0909248bbcd045827513a041
fyrworx4 <fyrworx@gmail.com>
2021-08-29 15:51:44 -0700

    asdf

--- rdptest.py
+++ rdptest.py
@@ -19,31 +19,7 @@ class MyRDPFactory(rdp.ClientFactory):
                 #send 'r' key
                 self._controller.sendKeyEventUnicode(ord(unicode("r".toUtf8(), encoding="UTF-8")), True)
                 #mouse move and click at pixel 200x200
-                self._controller.sendPointerEvent(200, 200, 1, true)
-
-            def onUpdate(self, destLeft, destTop, destRight, destBottom, width, height, bitsPerPixel, isCompress, data):
-                """
-                @summary: Notify bitmap update
-                @param destLeft: xmin position
-                @param destTop: ymin position
-                @param destRight: xmax position because RDP can send bitmap with padding
-                @param destBottom: ymax position because RDP can send bitmap with padding
-                @param width: width of bitmap
-                @param height: height of bitmap
-                @param bitsPerPixel: number of bit per pixel
-                @param isCompress: use RLE compression
-                @param data: bitmap data
-                """
-                
-            def onSessionReady(self):
-		        """
-		        @summary: Windows session is ready
-		        """
-
-            def onClose(self):
-                """
-                @summary: Call when stack is close
-                """
+                self._controller.sendPointerEvent(200, 200, 1, True)
 
         return MyObserver(controller)
 
