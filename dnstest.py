2a5daa345c48628cb816af89061d85f81dfb4ba3
fyrworx4 <fyrworx@gmail.com>
2021-08-29 15:35:04 -0700

    jsadf

--- dnstest.py
+++ dnstest.py
@@ -3,7 +3,7 @@ from dns.resolver import *
 
 res = resolver.Resolver()
 
-answers = res.query('telcolab.xyz', 'A')
+answers = res.query('pfsense.telcolab.xyz', 'A')
 
 for ipval in answers:
     print('asdfasf', ipval.to_text())
