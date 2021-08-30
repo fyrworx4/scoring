9a443a0abbf67c68b0bdc6a1b62fe2b5ade00140
fyrworx4 <fyrworx@gmail.com>
2021-08-29 20:50:46 -0700

    asdf

--- rdptest.py
+++ rdptest.py
@@ -1,11 +1,11 @@
 import subprocess
 
-username = "saulsolperrrr"
+username = "saulsolper"
 password = "Orbitalweapons_1234"
 server = "10.100.10.145"
 port = "3389"
 
-cmd = ['xfreerdp', '--ignore-certificate', '--authonly', '-u', username, '-p', password]
-cmd.append('{}:{}'.format(server, port))
+cmd = ['xfreerdp', '--ignore-certificate', '--authonly', '/u:' + username, '/p:' + password, '/v:' + server]
+#cmd.append('{}:{}'.format(server, port))
 
 output = subprocess.check_output(cmd, stderr=subprocess.STDOUT)
