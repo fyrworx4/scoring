31202cc222027e7cc345d2a409d3d36f70d9448f
fyrworx4 <fyrworx@gmail.com>
2021-08-29 20:55:57 -0700

    asfd

--- rdptest.py
+++ rdptest.py
@@ -5,7 +5,7 @@ password = "Orbitalweapons_1234"
 server = "10.100.10.145"
 port = "3389"
 
-cmd = ['xfreerdp', '/cert-ignore', '/authonly', '/u:' + username, '/p:' + password, '/v:' + server]
+cmd = ['xfreerdp', '/cert-ignore', '/auth-only', '/u:' + username, '/p:' + password, '/v:' + server]
 #cmd.append('{}:{}'.format(server, port))
 
 output = subprocess.check_output(cmd, stderr=subprocess.STDOUT)
