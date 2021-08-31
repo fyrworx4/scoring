1376208da4363a86ec19130e69a1dcfacb0aeaec
Taylor Nguyen <fyrworx@gmail.com>
2021-08-30 23:31:21 -0700

    asdf

--- rdptest2.py
+++ rdptest2.py
@@ -4,7 +4,7 @@ def pollRDP(ip, port, user):
     username = user.split(":")[0]
     password = user.split(":")[1]
     cmd = ['xfreerdp', '--ignore-certificate', '--authonly', '-u', username, '-p', password, ip]
-    output = subprocess.check_output(cmd, stderr=subprocess.STDOUT)
+    output = subprocess.check_output(cmd, stderr=subprocess.STDERR)
     print(output)
 
 pollRDP("10.100.10.145", "3389", "saulsolper:Orbitalweapons_1234") # True
