7b13585b4a760a42cb3e96b8b542a0e7b9b03898
Taylor Nguyen <fyrworx@gmail.com>
2021-08-31 00:29:23 -0700

    asdfsadf

--- rdptest2.py
+++ rdptest2.py
@@ -7,8 +7,7 @@ def pollRDP(ip, port, user):
     #cmd = ['xfreerdp', '/cert-ignore', '/auth-only', f'/u:"{username}"', f'/p:"{password}"', f'/v:{ip}']
 
     proc = subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-    output = proc.stdout.read()
- 
+    output = proc.stdout.communicate()
     print(output)
     #p = subprocess.Popen(cmd, stdout=subprocess.PIPE, close_fds=True)
     #output = p.stdout.read()
