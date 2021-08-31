f4c45bb46b474455ded368b17ca18a872e0e66b4
Taylor Nguyen <fyrworx@gmail.com>
2021-08-30 22:20:06 -0700

    asdf

---
+++ rdptest2.py
@@ -0,0 +1,13 @@
+def pollRDP(ip, port, user):
+    try:
+        for user in users:
+            if ":" not in user:
+                continue
+            username = user.split(":")[0]
+            password = user.split(":")[1]
+        cmd = ['xfreerdp', '/cert-ignore', '/auth-only', '/u:"' + username, '" /p:"' + password, '" /v:' + ip]
+        return True
+    except:
+        return False
+
+pollRDP("10.100.10.145", "3389", "saulsolper:Orbitalweapons_1234")
