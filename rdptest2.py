590d9037c337bfe9312add37f3d461560c2f616e
Taylor Nguyen <fyrworx@gmail.com>
2021-08-30 22:53:38 -0700

    hahahahaha

--- rdptest2.py
+++ rdptest2.py
@@ -2,10 +2,9 @@ import subprocess
 
 def pollRDP(ip, port, user):
     username = user.split(":")[0]
-    print(username)
     password = user.split(":")[1]
-    print(password)
     cmd = ['xfreerdp', '/cert-ignore', '/auth-only', '/u:"' + username + '"',  '/p:"' + password + '"', '/v:' + ip]
+    # xfreerdp /cert-ignore /auth-only /u:"saulsolper" /p:"Orbitalweapons_1234" /v:10.100.10.145
     output = subprocess.check_output(cmd, stderr=subprocess.STDOUT)
     print(output)
 
