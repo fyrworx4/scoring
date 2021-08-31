f85fa2940f9cdcd2357381a36662d58c6526d79c
Taylor Nguyen <fyrworx@gmail.com>
2021-08-30 23:42:06 -0700

    asdf

--- rdptest2.py
+++ rdptest2.py
@@ -7,5 +7,5 @@ def pollRDP(ip, port, user):
     running = subprocess.Popen(cmd, stdout=subprocess.PIPE)
     print(running.stdout.read())
 
-pollRDP("10.100.10.145", "3389", "saulsolper:Orbitalweapons_1234") # True
-#pollRDP("10.100.10.135", "3389", "saulsolper:Orbitalweapons_1234") # False
+#pollRDP("10.100.10.145", "3389", "saulsolper:Orbitalweapons_1234") # True
+pollRDP("10.100.10.135", "3389", "saulsolper:Orbitalweapons_1234") # False
