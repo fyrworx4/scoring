0b0192cbbe8617d44f5b16a957d7f844fe68bb9a
fyrworx4 <fyrworx@gmail.com>
2021-10-24 16:37:41 -0700

    fixed stuff

--- mysqltest.py
+++ mysqltest.py
@@ -4,24 +4,21 @@ Description: Will verify that the mySQL service is running on the specific port 
 Parameters: ip - ip address to poll, port - port number to poll, users - list of users to connect with, databaseName - name of database to use, tableName - name of table to use, tableHash - hash of table
 """
 
+import mysql.connector
+import hashlib
+
 ip = "10.100.10.200"
 username = "jarjarbinks"
-password = "rebel#1"
+pw = "rebel#1"
 databaseName = "hologram"
 tableName = "quotes"
 tableHash = "f0c280f803ffa668866b113f453194e5"
 
 try:
-    for user in users:
-        if ":" not in user:
-            continue
-        username = user.split(":")[0]
-        password = user.split(":")[1]
-
     mydb = mysql.connector.connect(
         host = ip,
         user = username,
-        password = password,
+        password = pw,
         database = databaseName
     )
 
@@ -33,11 +30,8 @@ try:
     output = str(output).encode("utf-8")
     
     if(hashlib.md5(output).hexdigest() == tableHash):
-        return True
         print("True")
     else:
-        return False
         print("False")
 except:
-    return False
-    print("False)")
+    print("False")
